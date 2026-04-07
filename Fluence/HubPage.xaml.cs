aef644e2dea7445277f577a9f69ab856f25ae85e
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2026-04-07 19:59:19 +0800

    feat: user can delete a transaction

--- Fluence/HubPage.xaml.cs
+++ Fluence/HubPage.xaml.cs
@@ -108,6 +108,31 @@ namespace Fluence
             }
         }
 
+        private async void DeleteTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            var menuItem = sender as MenuFlyoutItem;
+            var displayItem = menuItem.DataContext as TransactionDisplayItem;
+            if (displayItem != null)
+            {
+                var dialog = new Windows.UI.Popups.MessageDialog("are you sure you want to delete this transaction?", "confirm delete");
+                dialog.Commands.Add(new Windows.UI.Popups.UICommand("delete") { Id = 0 });
+                dialog.Commands.Add(new Windows.UI.Popups.UICommand("cancel") { Id = 1 });
+                dialog.DefaultCommandIndex = 1;
+
+                var result = await dialog.ShowAsync();
+                if ((int)result.Id == 0)
+                {
+                    var transactionViewModel = new TransactionViewModel();
+                    if (await transactionViewModel.DeleteTransactionAsync(displayItem.Id))
+                    {
+                        await this.hubViewModel.LoadOverviewAsync();
+                        await this.hubViewModel.LoadHistoryAsync();
+                        await this.hubViewModel.LoadReportAsync();
+                    }
+                }
+            }
+        }
+
         private void EditProfileAppBarButton_Click(object sender, RoutedEventArgs e)
         {
             if (!Frame.Navigate(typeof(ProfilePage)))
