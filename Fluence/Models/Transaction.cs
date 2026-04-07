c8c7613df439c76bf39bb605b672991c94a483a2
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2026-04-07 16:55:16 +0800

    feat: create model Transaction

---
+++ Fluence/Models/Transaction.cs
@@ -0,0 +1,91 @@
+using SQLite;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Fluence.Models
+{
+    public class Transaction : INotifyPropertyChanged
+    {
+        private int _categoryId;
+        private string _note;
+        private DateTime _date = DateTime.Now;
+        private double _amount;
+        private string _type;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public int CategoryId
+        {
+            get { return _categoryId; }
+            set
+            {
+                if (_categoryId != value)
+                {
+                    _categoryId = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Note
+        {
+            get { return _note; }
+            set
+            {
+                if (_note != value)
+                {
+                    _note = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                if (_date != value)
+                {
+                    _date = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set
+            {
+                if (_amount != value)
+                {
+                    _amount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
